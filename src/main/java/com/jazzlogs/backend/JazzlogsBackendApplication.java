6634a2b6f06e08b47c76455826ec96b53bc288ab
marco romano fina <marco@Mac.fibertel.com.ar>
2026-07-24 16:36:21 -0300

    initial commit for jazzlogs system refacoring

---
+++ src/main/java/com/jazzlogs/backend/JazzlogsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jazzlogs.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazzlogsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazzlogsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jazzlogs/backend/JazzlogsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jazzlogs.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazzlogsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
